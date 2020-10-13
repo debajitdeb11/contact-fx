e1991fbc24a5a694d428a132622c374d15bb4ff4
Debajit Deb <debajitdeb11@gmail.com>
2020-10-13 06:25:36 +0530

    Project init

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module ContactFX.Challenge {
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+
+    opens xyz.knowdeb.ContactFX;
+}
---
+++ src/xyz/knowdeb/ContactFX/Controller.java
@@ -0,0 +1,4 @@
+package xyz.knowdeb.ContactFX;
+
+public class Controller {
+}
